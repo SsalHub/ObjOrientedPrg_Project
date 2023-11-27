0ac084dfb5bc2715e8fda8861a404b99461834df
jjjcastle <149080092+jjjcastle@users.noreply.github.com>
2023-11-27 20:53:57 +0900

    feat:파일 재생성 및 Event,Task data class 정의

--- src/main/kotlin/Event.kt
+++ src/main/kotlin/Event.kt
@@ -1,3 +1,8 @@
-data class Event{
-
-}
+import java.io.Serializable
+data class Event(
+    val id : Int,
+    var title : String,
+    val beginTime : Int,
+    val endTime : Int,
+    var detail:String
+) : Serializable
--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,3 +1,8 @@
-data class Task{
+import java.io.Serializable
 
-}
+data class Task(
+    val id : Int,
+    var title : String,
+    val beginTime : Int,
+    var detail : String
+) : Serializable
